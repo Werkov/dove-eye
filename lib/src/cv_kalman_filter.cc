22f2e710f1433452de246749f26ebdf366fee5dd
Michal Koutný <mkoutny@suse.com>
2016-06-28 17:45:39 +0200

    Fix compilation on Leap 42.1
    - disable background subtractor
    - matrix initialization types

--- lib/src/cv_kalman_filter.cc
+++ lib/src/cv_kalman_filter.cc
@@ -18,7 +18,7 @@ CvKalmanFilter::CvKalmanFilter(const CvKalmanFilter &other)
 
 void CvKalmanFilter::Init(const double process_var, const double observation_var) {
   
-  kalman_filter_.transitionMatrix = *(cv::Mat_<MatType>(4, 4) <<
+  kalman_filter_.transitionMatrix = (cv::Mat_<MatType>(4, 4) <<
                                       1, 0, 1, 0,
                                       0, 1, 0, 1,
                                       0, 0, 1, 0,
