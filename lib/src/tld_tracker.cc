7a5d4affbe998793b89f3bde648e1fca4e46bb2a
Michal Koutný <mkoutny@suse.com>
2016-06-30 18:27:02 +0200

    Coding style

--- lib/src/tld_tracker.cc
+++ lib/src/tld_tracker.cc
@@ -3,109 +3,101 @@
 #include <tld/TLD.h>
 
 namespace dove_eye {
-	bool TldTracker::InitTrackerData(const cv::Mat &data, const Mark &mark) {
-		assert(mark.type == Mark::kRectangle);
-		tld = new tld::TLD();
-		tracker_data();
-		data_.grey = cv::Mat(data.rows, data.cols, CV_8UC1);
+  bool TldTracker::InitTrackerData(const cv::Mat &data, const Mark &mark) {
+    assert(mark.type == Mark::kRectangle);
+    tld = new tld::TLD();
+    tracker_data();
+    data_.grey = cv::Mat(data.rows, data.cols, CV_8UC1);
 
-		return true;
-	}
+    return true;
+  }
 
-	bool TldTracker::InitializeTracking(const Frame &frame, const Mark mark,
-		Posit *result) {
+  bool TldTracker::InitializeTracking(const Frame &frame, const Mark mark,
+    Posit *result) {
 
-		if (!InitTrackerData(frame.data, mark)) {
-			return false;
-		}
-		
-		initialized(true);
+    if (!InitTrackerData(frame.data, mark)) {
+      return false;
+    }
 
-		cvtColor(frame.data, data_.grey, CV_BGR2GRAY);
+    initialized(true);
 
-		tld->detectorCascade->imgWidth = data_.grey.cols;
-		tld->detectorCascade->imgHeight = data_.grey.rows;
-		tld->detectorCascade->imgWidthStep = data_.grey.step;
+    cvtColor(frame.data, data_.grey, CV_BGR2GRAY);
 
-		cv::Rect bb(mark.top_left, mark.size);
-		tld->selectObject(data_.grey, &bb);
+    tld->detectorCascade->imgWidth = data_.grey.cols;
+    tld->detectorCascade->imgHeight = data_.grey.rows;
+    tld->detectorCascade->imgWidthStep = data_.grey.step;
 
-		*result = MarkToPosit(mark);
+    cv::Rect bb(mark.top_left, mark.size);
+    tld->selectObject(data_.grey, &bb);
 
-		return true;
-	}
+    *result = MarkToPosit(mark);
 
-	bool TldTracker::InitializeTracking(
-		const Frame &frame,
-		const Epiline epiline,
-		const TrackerData &tracker_data,
-		Posit *result) {
+    return true;
+  }
 
-		auto thickness = parameters().Get(Parameters::TEMPLATE_RADIUS) *
-			parameters().Get(Parameters::SEARCH_FACTOR);
+  bool TldTracker::InitializeTracking(
+    const Frame &frame,
+    const Epiline epiline,
+    const TrackerData &tracker_data,
+    Posit *result) {
 
-		const auto epiline_mask = EpilineToMask(frame.data.size(), thickness, epiline);
+    auto thickness = parameters().Get(Parameters::TEMPLATE_RADIUS) *
+      parameters().Get(Parameters::SEARCH_FACTOR);
 
-		Mark match_mark;
-		//// TODO Remove non-const cast! Do it properly when projection is working
-		//if (!Search(frame.data, (TrackerData &)tracker_data, nullptr, &epiline_mask, thr,
-		//	&match_mark)) {
-		//	return false;
-		//}
+    const auto epiline_mask = EpilineToMask(frame.data.size(), thickness, epiline);
 
-		if (!InitTrackerData(frame.data, match_mark)) {
-			return false;
-		}
-		initialized(true);
+    Mark match_mark;
+    //// TODO Remove non-const cast! Do it properly when projection is working
+    //if (!Search(frame.data, (TrackerData &)tracker_data, nullptr, &epiline_mask, thr,
+    //  &match_mark)) {
+    //  return false;
+    //}
 
-		*result = MarkToPosit(match_mark);
+    if (!InitTrackerData(frame.data, match_mark)) {
+      return false;
+    }
+    initialized(true);
 
-		return true;
-	}
+    *result = MarkToPosit(match_mark);
 
-	bool TldTracker::Track(const Frame &frame, Posit *result) {
-		assert(initialized());
+    return true;
+  }
 
-		return RunTracking(frame, result);
-	}
+  bool TldTracker::Track(const Frame &frame, Posit *result) {
+    assert(initialized());
 
-	bool TldTracker::ReinitializeTracking(const Frame &frame, Posit *result) {
-		assert(initialized());
+    return RunTracking(frame, result);
+  }
 
-		return RunTracking(frame, result);
-	}
+  bool TldTracker::ReinitializeTracking(const Frame &frame, Posit *result) {
+    assert(initialized());
 
-	bool TldTracker::RunTracking(const Frame &frame, Posit *result) {
-		
-		if (!data_.reuseFrameOnce)
-		{
-			cvtColor(frame.data, data_.grey, CV_BGR2GRAY);
-		}
-		else
-		{
-			data_.reuseFrameOnce = false;
-		}
+    return RunTracking(frame, result);
+  }
 
-		if (!data_.skipProcessingOnce)
-		{
-			tld->processImage(frame.data);
-		}
-		else
-		{
-			data_.skipProcessingOnce = false;
-		}
+  bool TldTracker::RunTracking(const Frame &frame, Posit *result) {
+    if (!data_.reuseFrameOnce) {
+      cvtColor(frame.data, data_.grey, CV_BGR2GRAY);
+    } else {
+      data_.reuseFrameOnce = false;
+    }
 
-		if (tld->currConf < data_.threshold)
-			return false;
-			
+    if (!data_.skipProcessingOnce) {
+      tld->processImage(frame.data);
+    } else {
+      data_.skipProcessingOnce = false;
+    }
 
+    if (tld->currConf < data_.threshold) {
+      return false;
+    }
 
-		Mark mark;
-		mark.top_left.x = tld->currBB->x;
-		mark.top_left.y = tld->currBB->y;
-		mark.size.x = tld->currBB->width;
-		mark.size.x = tld->currBB->height;
+    Mark mark;
+    mark.top_left.x = tld->currBB->x;
+    mark.top_left.y = tld->currBB->y;
+    mark.size.x = tld->currBB->width;
+    mark.size.x = tld->currBB->height;
 
-		*result = MarkToPosit(mark);
-	}
-}
+    *result = MarkToPosit(mark);
+  }
+} // namespace dove_eye
